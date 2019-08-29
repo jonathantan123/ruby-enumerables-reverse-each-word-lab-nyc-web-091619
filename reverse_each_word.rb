21865a9cda68ef170a7495a58a42bf19323d722b
Jonathan Tan <jonathantan@brandeis.edu>
2019-08-29 17:37:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,13 @@
 def reverse_each_word (sentence1)
 new_array = [] 
+sentence1.split.
+new_array << sentence1.split.each {|x| x.reverse }
 
+new_array.join(" ")
 
 # sentence1.split.collect {|x| x.reverse! }.join(" ")
 
-new_array << sentence1.split.each {|x| x.reverse }
-new_array.join(" ")
+
 
 end
 
