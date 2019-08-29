595350dcc4ee96376072fce78f67f1b08475f4f1
Jonathan Tan <jonathantan@brandeis.edu>
2019-08-29 17:19:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word (sentence1)
 
 
-# sentence1.split.collect {|x| x.reverse! }
+sentence1.split.collect {|x| x.reverse! }
 sentence1.split.each {|x| x.reverse! }.join(" ")
 
 end
